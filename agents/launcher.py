05ca7abade1a57cef59b3e48abd9801da6f9b7dc
mkabtoul <kabtoul.maria@gmail.com>
2026-03-30 16:08:24 +0200

    (feature) Adds internal events setup to cortex launch

--- agents/launcher.py
+++ agents/launcher.py
@@ -1,8 +1,13 @@
+from launch.action import Action as ROSLaunchAction
+from launch.actions import RegisterEventHandler
+
 from ros_sugar import Launcher as BaseLauncher
 from ros_sugar.core.component import BaseComponent
 from ros_sugar.core.monitor import Monitor
+from ros_sugar.core.event import OnInternalEvent
+from ros_sugar.core.action import Action
 from ros_sugar.launch.launch_actions import ComponentLaunchAction
-from typing import List, Optional
+from typing import List, Optional, Dict, Union
 
 
 class Launcher(BaseLauncher):
@@ -10,6 +15,45 @@ class Launcher(BaseLauncher):
         self, namespace="", config_file=None, activation_timeout=None, robot_plugin=None
     ):
         super().__init__(namespace, config_file, activation_timeout, robot_plugin)
+
+    def _setup_additional_internal_actions(
+        self,
+        additional_internal_actions: Dict[str, List[Union[ROSLaunchAction, Action]]],
+    ) -> None:
+        # Add event handling actions
+        entities_dict: Dict = {}
+
+        for event_name, action_set in self._ros_events_actions.items():
+            for action in action_set:
+                if isinstance(action, ROSLaunchAction):
+                    entities_dict[event_name].append(action)
+
+                # Check action type
+                elif action._is_lifecycle_action:
+                    # Re-parse action for component related actions
+                    entities = self._get_action_launch_entity(action)
+                    if isinstance(entities, list):
+                        entities_dict[event_name].extend(entities)
+                    else:
+                        entities_dict[event_name].append(entities)
+
+                elif isinstance(action, Action) and action.parent_component:
+                    continue  # skip, as it will be added directly in the cortex monitor configuration
+
+                # If the action is not related to a component -> add opaque executable to launch
+                else:
+                    entities_dict[event_name].append(
+                        action.launch_action(monitor_node=self.monitor_node)
+                    )
+
+            # Register a new internal event handler
+            internal_events_handler = RegisterEventHandler(
+                OnInternalEvent(
+                    internal_event_name=event_name,
+                    entities=entities_dict[event_name],
+                )
+            )
+            self._description.add_action(internal_events_handler)
 
     def _init_monitor_node(
         self,
@@ -64,3 +108,8 @@ class Launcher(BaseLauncher):
             name=self.monitor_node.node_name,
         )
         self._description.add_action(monitor_action)
+
+        # Add any additional internal actions related to the monitor (e.g. events handling actions)
+        self._setup_additional_internal_actions(
+            self.monitor_node._additional_internal_actions
+        )
